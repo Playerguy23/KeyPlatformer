a7293e4debc1c957ea4fc1ad47b05b5ca3382db2
playerguy23 <maa1kivi@gmail.com>
2018-06-27 00:49:19 +0300

    keyPlatformer1.0

---
+++ KeyPlatformer1.0/Assets/scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    private PlayerController player;
+
+    public GameObject checkPoint;
+    public GameObject dethPartical;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void respawn() {
+        StartCoroutine("StartRespawn");
+    }
+
+    public IEnumerator StartRespawn() {
+        Instantiate(dethPartical, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSeconds(1);
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        player.transform.position = checkPoint.transform.position;
+    }
+}
---
+++ KeyPlatformer1.0/Assets/scripts/playerScripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    // speeds
+    private float moveSpeed;
+    private float JumpSpeed;
+
+    // rigidbody
+    private Rigidbody2D player;
+
+    // ground check
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        moveSpeed = CrossPlatformInputManager.GetAxis("Horizontal");
+        JumpSpeed = CrossPlatformInputManager.GetAxis("Vertical");
+
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * 10, player.velocity.y);
+
+        if(grounded)
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.velocity.x, JumpSpeed * 15);
+    }
+}
